79b8ea1c3ee3566ff283af1029e3d30441ec6845
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-12-08 18:05:55 +0300

    Map @ElementCollection annotation

--- 01-collections-set/src/main/java/com/luv2code/entity/Student.java
+++ 01-collections-set/src/main/java/com/luv2code/entity/Student.java
@@ -1,10 +1,13 @@
 package com.luv2code.entity;
 
+import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -34,6 +37,9 @@ public class Student {
     @Column(name = "email")
     private String email;
 
+    @ElementCollection
+    @CollectionTable(name = "image",
+            joinColumns = @JoinColumn(name = "student_id"))
     @Column(name = "file_name")
     private Set<String> images = new HashSet<>();
 
