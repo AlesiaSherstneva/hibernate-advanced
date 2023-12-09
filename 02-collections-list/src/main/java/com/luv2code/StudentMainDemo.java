6a6f373da83ab1ba5a16bd107eae58be6f0a3c4f
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-12-09 21:08:36 +0300

    Copy, paste and rename previous project for new theme

---
+++ 02-collections-list/src/main/java/com/luv2code/StudentMainDemo.java
@@ -0,0 +1,33 @@
+package com.luv2code;
+
+import com.luv2code.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.Set;
+
+public class StudentMainDemo {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+        try (factory; Session session = factory.getCurrentSession()) {
+            Student tempStudent = new Student("Paul", "Wall", "paul@luv2code.com");
+
+            Set<String> theImages = tempStudent.getImages();
+            theImages.add("photo1.jpg");
+            theImages.add("photo2.jpg");
+            theImages.add("photo3.jpg");
+            theImages.add("photo4.jpg");
+
+            session.beginTransaction();
+            System.out.println("Saving the student and images...");
+            session.persist(tempStudent);
+            session.getTransaction().commit();
+            System.out.println("Done!!!");
+        }
+    }
+}
---
+++ 02-collections-list/src/main/java/com/luv2code/entity/Student.java
@@ -0,0 +1,51 @@
+package com.luv2code.entity;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "student")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @ElementCollection
+    @CollectionTable(name = "image",
+            joinColumns = @JoinColumn(name = "student_id"))
+    @Column(name = "file_name")
+    private Set<String> images = new HashSet<>();
+
+    public Student(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+}
