90a6da43b42d0bd99d91d738915c0f9714f3a826
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-03-22 21:52:49 +0300

    Update main app to retrieving a student with his address

--- 06-embeddable/src/main/java/com/luv2code/StudentMainDemo.java
+++ 06-embeddable/src/main/java/com/luv2code/StudentMainDemo.java
@@ -1,31 +1,26 @@
 package com.luv2code;
 
+import com.luv2code.entity.Address;
 import com.luv2code.entity.Student;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-
-import java.util.List;
 
 public class StudentMainDemo {
     public static void main(String[] args) {
         SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Student.class)
+                .addAnnotatedClass(Address.class)
                 .buildSessionFactory();
 
         try (factory; Session session = factory.getCurrentSession()) {
             Student tempStudent = new Student("John", "Doe", "john@luv2code.com");
-
-            List<String> theImages = tempStudent.getImages();
-            theImages.add("photo1.jpg");
-            theImages.add("photo2.jpg");
-            theImages.add("photo3.jpg");
-            theImages.add("photo4.jpg");
-            theImages.add("photo4.jpg");
+            Address homeAddress = new Address("Some Street", "Some City", "12345");
 
             session.beginTransaction();
-            System.out.println("Saving the student and images...");
+            System.out.println("Saving the student and address...");
+            tempStudent.setHomeAddress(homeAddress);
             session.persist(tempStudent);
             session.getTransaction().commit();
             System.out.println("Done!!!");
