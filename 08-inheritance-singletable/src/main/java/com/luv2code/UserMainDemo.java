f0c1d512636329e888f447a5baef11494fb29570
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-03-28 22:51:46 +0300

    Update main class to retrieving all kings of users

--- 08-inheritance-singletable/src/main/java/com/luv2code/UserMainDemo.java
+++ 08-inheritance-singletable/src/main/java/com/luv2code/UserMainDemo.java
@@ -1,26 +1,27 @@
 package com.luv2code;
 
-import com.luv2code.entity.Status;
+import com.luv2code.entity.Instructor;
 import com.luv2code.entity.Student;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class StudentMainDemo {
+public class UserMainDemo {
     public static void main(String[] args) {
         SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Student.class)
+                .addAnnotatedClass(Instructor.class)
                 .buildSessionFactory();
 
         try (factory; Session session = factory.getCurrentSession()) {
-            Student tempStudent1 = new Student("John", "Doe", "john@luv2code.com", Status.ACTIVE);
-            Student tempStudent2 = new Student("Mary", "Public", "mary@luv2code.com", Status.INACTIVE);
+            Student tempStudent = new Student("John", "Doe", "john@luv2code.com", "Hibernate");
+            Instructor tempInstructor = new Instructor("Mary", "Public", "mary@luv2code.com", 20000.00);
 
             session.beginTransaction();
-            System.out.println("Saving students...");
-            session.persist(tempStudent1);
-            session.persist(tempStudent2);
+            System.out.println("Saving users...");
+            session.persist(tempStudent);
+            session.persist(tempInstructor);
             session.getTransaction().commit();
             System.out.println("Done!!!");
         }
